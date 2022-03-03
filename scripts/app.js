fa46a9eb1ab268e939a15d6e77b0fc11fb001bd5
DaltonHart <daltonvaren@gmail.com>
2022-03-03 15:17:33 -0800

    adds scripts and styles

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+/**
+ * TODO
+ * - History of versioning software 
+ * - Roles of Git vs Github 
+ * - Command Commands of git
+ * - gitignore
+ * - commit formatting tool (commitzen)
+ * 
+ * == Git Commands ==  
+ * One time commands
+ *    - init 
+ *    - remote (depends on setup)
+ * 
+ * Work flow commands
+ *      - add -> two things -- add to the next commit (staged) + init tracking
+ *      - commit -> creates a point in the timeline
+ *      - push 
+ *      - pull 
+ *      - branch 
+ *      - checkout 
+ *      - status
+ *      - log
+ * 
+ * Practice Peer Workflow with Forks
+ *    - fork 
+ *    - set upstream 
+ * -----------------------------------
+ */
